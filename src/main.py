8fbcf027ee809c75a7ffb780a07f98308f07d55b
New Darius <ndarius@dso.org.sg>
2024-03-18 21:16:35 +0800

    fixed the compare to show both a* and rrt diagrams

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 from timeit import default_timer as timer
 from algorithm import aStarPlanner
+from algorithm import rrtPlanner
 from world import World
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 from matplotlib.animation import FuncAnimation
+import copy
 
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Planner configuration')
-    parser.add_argument('-p', '--planner', dest='planner', type=str, choices=['a*', 'rrt'], default='a*', help='[a*, rrt] (default: a*)')
+    parser.add_argument('-p', '--planner', dest='planner', type=str, choices=['a*', 'rrt', 'compare'], default='compare', help='[a*, rrt, compare] (default: compare)')
     parser.add_argument('-v', '--visualise', dest='visualise', type=int, choices=[0,1], default=1, help='[0 for no vis, 1 for vis] (default: 1)')
     args = parser.parse_args()
 
-    world = World(100,100)
+    world = World(50,50)
     world.vis()
  
     if args.planner == 'a*':
@@ -22,25 +24,82 @@ if __name__ == "__main__":
         end = timer()
         print("time taken: ", end-start)
         world.vis()
+
+        if args.visualise == 1:
+            cmap = ListedColormap(['w', 'k', 'r', 'b', 'y', 'c' ,'g'])
+            fig = plt.figure()
+            plot = plt.matshow(world.frames[0], cmap=cmap, fignum=0)
+
+            def init():
+                plot.set_data(world.frames[0])
+                return plot
+
+            def update(i):
+                plot.set_data(world.frames[i])
+                return [plot]
+            anim = FuncAnimation(fig, update, init_func=init, frames = len(world.frames), interval = 30, repeat=False)
+            plt.show()
+        
     elif args.planner == 'rrt':
         start = timer()
-        aStarPlanner(world) #change to rrt
+        rrtPlanner(world) #change to rrt
         end = timer()
         print("time taken: ", end-start)
         world.vis()
-    if args.visualise == 1:
-        cmap = ListedColormap(['w', 'k', 'r', 'b', 'y', 'g'])
-        fig = plt.figure()
-        plot = plt.matshow(world.frames[0], cmap=cmap, fignum=0)
 
-        def init():
-            plot.set_data(world.frames[0])
-            return plot
+        if args.visualise == 1:
+            cmap = ListedColormap(['w', 'k', 'r', 'b', 'y', 'c' ,'g'])
+            fig = plt.figure()
+            plot = plt.matshow(world.frames[0], cmap=cmap, fignum=0)
 
-        def update(i):
-            plot.set_data(world.frames[i])
-            return [plot]
-        anim = FuncAnimation(fig, update, init_func=init, frames = len(world.frames), interval = 30)
-        plt.show()
+            def init():
+                plot.set_data(world.frames[0])
+                return plot
+
+            def update(i):
+                plot.set_data(world.frames[i])
+                return [plot]
+            anim = FuncAnimation(fig, update, init_func=init, frames = len(world.frames), interval = 30, repeat=False)
+            plt.show()
+
+    elif args.planner == 'compare':
+        aStarWorld = copy.deepcopy(world)
+        aStarStart = timer()
+        aStarPlanner(aStarWorld)
+        aStarEnd = timer()
+        print("a*: ", aStarEnd-aStarStart)
+        
+        rrtWorld = copy.deepcopy(world)
+        rrtStart = timer()
+        rrtPlanner(rrtWorld)
+        rrtEnd = timer()
+        print("rrt: ", rrtEnd-aStarStart)
+
+        if args.visualise == 1:
+            cmap = ListedColormap(['w', 'k', 'r', 'b', 'y', 'c' ,'g'])
+            fig1 = plt.figure(1)
+            plot1 = plt.matshow(aStarWorld.frames[0], cmap=cmap, fignum=0)
+
+            def init1():
+                plot1.set_data(aStarWorld.frames[0])
+                return plot1
+
+            def update1(i):
+                plot1.set_data(aStarWorld.frames[i])
+                return [plot1]
+            anim1 = FuncAnimation(fig1, update1, init_func=init1, frames = len(aStarWorld.frames), interval = 30, repeat=False)
+            
+            fig2 = plt.figure(2)
+            plot2 = plt.matshow(rrtWorld.frames[0], cmap=cmap, fignum=0)
+
+            def init2():
+                plot2.set_data(rrtWorld.frames[0])
+                return plot2
+
+            def update2(i):
+                plot2.set_data(rrtWorld.frames[i])
+                return [plot2]
+            anim2 = FuncAnimation(fig2, update2, init_func=init2, frames = len(rrtWorld.frames), interval = 30, repeat=False)
+            plt.show()
 
     print('exit')
